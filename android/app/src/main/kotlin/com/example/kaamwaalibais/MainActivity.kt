e66bd75da75645cf8774cb8d726fd2552aec0132
Ritesh-237 <ritesh.dixit2002@gmail.com>
2025-07-30 09:19:27 +0530

    increments

--- android/app/src/main/kotlin/com/example/kaamwaalibais/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/kaamwaalibais/MainActivity.kt
@@ -2,4 +2,5 @@ package com.innowrap.user.kaamwalibais
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterFragmentActivity() {
+}
