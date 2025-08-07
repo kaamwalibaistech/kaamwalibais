3f2abce16b854d4c1afbf118b3f37ad14d103f41
anil-s-yadav <paytmking15@gmail.com>
2025-08-07 10:10:04 +0530

    increment

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:kaamwaalibais/Navigation_folder/navigation_screen.dart';
+import 'package:kaamwaalibais/utils/font.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,10 +11,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    TextTheme textTheme = createTextTheme(context, "Poppins", "Poppins");
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Kaamwalibais',
       theme: ThemeData(
+        textTheme: textTheme,
         colorScheme: ColorScheme.fromSeed(
           primary: Color(0xff7400b9),
           seedColor: Color(0xff7400b9),
